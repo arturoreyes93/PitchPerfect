6593df5ae230aa51a2cab4d53401b54ea9809c12
Arturo Reyes <reyesm93@gmail.com>
2017-11-22 17:28:52 -0600

    Migrate to Swift 4

--- PitchPerfect/PlaySoundsViewController+Audio.swift
+++ PitchPerfect/PlaySoundsViewController+Audio.swift
@@ -118,7 +118,7 @@ extension PlaySoundsViewController: AVAudioPlayerDelegate {
         }
     }
     
-    func stopAudio() {
+    @objc func stopAudio() {
         
         if let stopTimer = stopTimer {
             stopTimer.invalidate()
